555b2302634fe69fd9770e71f6731ab6f8a2a0b0
Matthew Rizzini <matthewrizzini@gmail.com>
2018-03-04 21:41:52 +0000

    BTC price displaying on main.html

--- public/angular-app/search/search-controller.js
+++ public/angular-app/search/search-controller.js
@@ -3,14 +3,11 @@ angular.module('meanNASDAQ').controller('SearchController', SearchController);
 
 function SearchController(stockDataFactory, $route, $window, AuthFactory, jwtHelper) {
     var vm = this;
-//   var token = $window.sessionStorage.token; // capturing token from session storage
-//     var decodedToken = jwtHelper.decodeToken(token); //decodes token 
-//     vm.loggedInUser = decodedToken.username; // add logged in user property so we can
     vm.isLoggedIn = function() {
         if (AuthFactory.isLoggedIn) {
-              var token = $window.sessionStorage.token; // capturing token from session storage
-    var decodedToken = jwtHelper.decodeToken(token); //decodes token 
-    vm.loggedInUser = decodedToken.username; // add logged in user property so we can
+            var token = $window.sessionStorage.token; // capturing token from session storage
+            var decodedToken = jwtHelper.decodeToken(token); //decodes token 
+            vm.loggedInUser = decodedToken.username; // add logged in user property so we can
             return true;
         } else {
             return false;
@@ -18,8 +15,8 @@ function SearchController(stockDataFactory, $route, $window, AuthFactory, jwtHel
     };
     
     vm.search = function() {
-    var symbol =  vm.symbol.toUpperCase();
-    var userSearch = {
+        var symbol =  vm.symbol.toUpperCase();
+        var userSearch = {
         symbol: vm.symbol.toUpperCase()
     };
     var user = vm.username;
@@ -75,81 +72,59 @@ function SearchController(stockDataFactory, $route, $window, AuthFactory, jwtHel
     }; // Ends vm.search()
 
 
-$(document).ready(function() {
-    
-    $.ajax({
-		    method: "GET",
-		    url: "https://newsapi.org/v2/top-headlines",
-		    data: { category: "business", country: "us", language: "en", apiKey: APIKEY },
-		    success: function(data) {
-			     if (data.status == "ok") {
-					console.log(data);
-				    for (var i = 0; i < data.articles.length; i++) {
-				    var headline = data.articles[i].title;      
-					var articles = document.createElement("LI");
-	                var anchor = document.createElement("A");
-	                anchor.setAttribute('href', data.articles[i].url);
-	                anchor.setAttribute('target', "_blank");
-	                anchor.innerHTML = headline;
-	                articles.setAttribute("id", "articles" + i);
-	                var description = document.createElement("P");
-					var descriptionText = data.articles[i].description || "Click the article for more information";
-				    description.innerHTML = "-" + descriptionText;
-				    document.getElementById("articlePaginate").appendChild(articles);
-				    document.getElementById("articles" + i).appendChild(anchor);
-				    document.getElementById("articlePaginate").appendChild(description);
-				    }
-				    $('#articlePaginate').easyPaginate({
-                    paginateElement: 'li',
-                    elementsPerPage: 5,
-                    effect: 'fade'
-                    });
-			        }
-		       }
-		       
-	
-	    });
-});
+
+    $(document).ready(function() {
+        $.ajax({
+    		    method: "GET",
+    		    url: "https://newsapi.org/v2/top-headlines",
+    		    data: { category: "business", country: "us", language: "en", apiKey: APIKEY },
+    		    success: function(data) {
+    			     if (data.status == "ok") {
+    					console.log(data);
+    				    for (var i = 0; i < data.articles.length; i++) {
+    				    var headline = data.articles[i].title;      
+    					var articles = document.createElement("LI");
+    	                var anchor = document.createElement("A");
+    	                anchor.setAttribute('href', data.articles[i].url);
+    	                anchor.setAttribute('target', "_blank");
+    	                anchor.innerHTML = headline;
+    	                articles.setAttribute("id", "articles" + i);
+    	                var description = document.createElement("P");
+    					var descriptionText = data.articles[i].description || "Click the article for more information";
+    				    description.innerHTML = "-" + descriptionText;
+    				    document.getElementById("articlePaginate").appendChild(articles);
+    				    document.getElementById("articles" + i).appendChild(anchor);
+    				    document.getElementById("articlePaginate").appendChild(description);
+    				    }
+    				    $('#articlePaginate').easyPaginate({
+                        paginateElement: 'li',
+                        elementsPerPage: 5,
+                        effect: 'fade'
+                        });
+    			        }
+    		        }
+    	    });
+    });
 
 
-        $(document).ready(function() {
-            $.ajax({
-        		    url: "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=USD&apikey=" + stockAPIKEY,
-        		    dataType: 'json', // https:www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=EUR&apikey=demo
-        		  //  data: { function: 'TIME_SERIES_INTRADAY', symbol: vm.symbol, interval: "1min", datatype: 'json', apikey: stockAPIKEY },
-        		    success: function(data) {
-        		        console.log('CRYPTO data is, ', data);
-        		        var currentDate = data["Meta Data"]["7. Last Refreshed"];
-        		        var apiData = data["Time Series (Digital Currency Intraday)"][currentDate];
-        		        var price = "1a. price (USD)";
-        		        console.log('price is ', price);
-        		        console.log('apiData is, ', apiData);
-        		        console.log('current date is, ', currentDate);
-        
-        		        var btcCurrentPrice = [apiData][price];
-        		        console.log('btcCurrentPrice is ', btcCurrentPrice);
-        		        
-        		
-        		      
-            //             console.log('current price should be 1078.92 ', currentPrice);
+    $(document).ready(function() {
+                $.ajax({
+            		    url: "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_INTRADAY&symbol=BTC&market=USD&apikey=" + stockAPIKEY,
+            		    dataType: 'json',
+            		    success: function(data) {
+            		        console.log('CRYPTO data is, ', data);
+            		        var currentDate = data["Meta Data"]["7. Last Refreshed"];
+            		      //  var apiData = data["Time Series (Digital Currency Intraday)"][currentDate];
+            		      //  var price = "1a. price (USD)";
+            		      //  var btcCurrentPrice = [apiData][price];
+            		      //  console.log('btcCurrentPrice is ', btcCurrentPrice);
+            		        var BTC = data["Time Series (Digital Currency Intraday)"][currentDate]["1a. price (USD)"];
+            		      //  console.log(BTC);
+            		      //  console.log(data["Time Series (Digital Currency Intraday)"][currentDate]["1a. price (USD)"]);
                             var currentBTC = document.createElement("SPAN");
-            				currentBTC.innerHTML = "$" + apiData;   //.slice(0, -2); 
-            				document.getElementById("currentBTCPrice").appendChild(currentBTC);
-        	           }
-        	    });
-        });
-
-// $('#articlePaginate').easyPaginate({
-//     paginateElement: 'li',
-//     elementsPerPage: 5,
-//     effect: 'climb'
-// });
-
-// $('#easyPaginate').easyPaginate({
-//     paginateElement: 'li',
-//     elementsPerPage: 3,
-//     effect: 'climb'
-// });
-
-
+                			currentBTC.innerHTML = "$" + BTC;   //.slice(0, -2); 
+                			document.getElementById("currentBTCPrice").appendChild(currentBTC);
+            	           }
+            	    });
+    });
 } // Ends SearchController 
